213456ad2f8f253618b0c3486697ebeaa05c02aa
danniramdhani <danniramdhani@proton.me>
2023-07-18 10:31:20 +0700

    Update contact form and placeholder message on contact page

--- app/(home)/contact/page.tsx
+++ app/(home)/contact/page.tsx
@@ -1,20 +1,21 @@
 'use client'
 
-import { ContactSection } from '@/components/contact/contact-section'
+import { ContactForm } from '@/components/contact/contact-form'
 
 export default function Contact() {
   return (
-    <div className='container pt-16 md:w-60'>
-      <div className='space-y-4 pb-4'>
+    <div className='container pb-8 pt-16 md:w-60'>
+      <div className='space-y-4'>
         <h1 className='font-heading text-3xl tracking-tight md:text-4xl'>
           Contact me
         </h1>
-        <p className='text-lg text-foreground-80 md:text-xl'>
-          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-          cillum sint consectetur cupidatat.
+        <p className='pb-2 text-lg text-foreground-80'>
+          Please feel free to contact me if you have any questions, whether
+          it&apos;s about work (I&apos;m available for hire!) or anything else
+          you&apos;d like to discuss.
         </p>
+        <ContactForm />
       </div>
-      <ContactSection />
     </div>
   )
 }
