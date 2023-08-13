31d9e2fdc7dba5d6d2fa7246a7762573c5294783
dannrs <danniramdhani@proton.me>
2023-08-13 09:40:39 +0700

    Remove dummy projects and add the real ones

--- config/projects.ts
+++ config/projects.ts
@@ -2,29 +2,10 @@ import { ProjectType } from '@/lib/types'
 
 export const projects: ProjectType[] = [
   {
-    name: 'NowPlayingLyrics',
-    coverImage: '/images/projects/NowPlayingLyrics.png',
+    name: 'portfolio',
     description:
-      'Automatic Romanized Lyrics Display for Currently Playing Songs',
-    githubUrl: 'https://github.com/dannrs/NowPlayingLyrics',
-    previewUrl: 'https://now-playing-lyrics.vercel.app/spotify',
-    tools: ['Next.js', 'Tailwind CSS']
+      'My personal portfolio site built with Next.js 13, TypeScript, Tailwind CSS and various other libraries',
+    githubUrl: 'https://github.com/dannrs/portfolio',
+    previewUrl: 'https://danny.my.id',
   },
-  {
-    name: 'Passphrase Generator',
-    coverImage: '/images/projects/PassphraseGenerator.png',
-    description: 'Random passphrase generator based on Diceware method',
-    githubUrl: 'https://gitlab.com/dxrs/passphrase-generator',
-    previewUrl: 'https://dxrs.gitlab.io/passphrase-generator/',
-    tools: ['React', 'Tailwind CSS']
-  },
-  {
-    name: 'Jaro-Winkler Component',
-    coverImage: '/images/projects/JaroWinkler.png',
-    description:
-      'A reusable React search component based on Jaro-Winkler Algorithm',
-    githubUrl: 'https://gitlab.com/dxrs/kbbi-daring',
-    previewUrl: 'https://dxrs.gitlab.io/kbbi-daring',
-    tools: ['React']
-  }
 ]
