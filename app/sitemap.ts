0262e70ec82235861a7ff027473e348e6de963a9
dannrs <danniramdhani@proton.me>
2023-09-12 10:49:51 +0700

    Add robot.ts and sitemap.ts files

---
+++ app/sitemap.ts
@@ -0,0 +1,38 @@
+import { allPosts } from 'contentlayer/generated'
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL as string
+  const postUrls = allPosts.map(post => ({
+    url: `${baseUrl}/blog/${post.slugAsParams}`,
+    lastModified: post.date
+  }))
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date()
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date()
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date()
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date()
+    },
+    {
+      url: `${baseUrl}/spotify`,
+      lastModified: new Date()
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date()
+    },
+    ...postUrls
+  ]
+}
